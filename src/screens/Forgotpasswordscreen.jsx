49dfb5723ce08fc85b3d225996fbb8ddd3ae8ab0
Zaeem Rehman <zaeemrehman39@gmail.com>
2026-01-05 15:35:15 +0500

    paused

--- src/screens/Forgotpasswordscreen.jsx
+++ src/screens/Forgotpasswordscreen.jsx
@@ -8,12 +8,25 @@ const Forgotpasswordscreen = () => {
    const navigation = useNavigation();
   
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container} >
 
       <TouchableOpacity
                   onPress={() => navigation.goBack()}>
                  <Icon name="chevron-back" size={26} color="#007AFF"/>
         </TouchableOpacity>
+
+        <View style={{marginTop:60}} >
+        <Text style={styles.title}>
+          Forgot <Text style={styles.highlight}>Password? </Text>
+        </Text>
+        <Text style={styles.subtitle}>
+          Don't Worry! It Occurs. Please Enter The Email Address 
+        </Text>
+        <Text style={styles.suubtitle} >
+          Linked With Your Account.
+        </Text>
+        </View>
+                 
 
     </SafeAreaView>
   )
@@ -21,4 +34,29 @@ const Forgotpasswordscreen = () => {
 
 export default Forgotpasswordscreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    padding: 20,
+  },
+  title: {
+    fontSize: 26,
+    fontWeight: '700',
+  },
+  highlight: {
+    color: '#1E88FF',
+  },
+  subtitle: {
+    fontSize: 14,
+    color: '#9A9A9A',
+    marginTop: 22,
+  },
+
+  suubtitle: {
+    fontSize: 14,
+    color: '#9A9A9A',
+  },
+
+})
